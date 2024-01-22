e06fb4d35c1e5cba295fcf29f7eba1edd79e9b5b
leocontreras559 <leonardlcontreras559@gmail.com>
2024-01-21 17:21:58 -0800

    hello world module 1

---
+++ module01a/VariablesandMath.js
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// Built-in Subroutines and Functions
+Math.sqrt(x);
+System.out.print( Math.sqrt(x) );
+lengthOfSide = Math.sqrt(x);
+// System.out.printf can be used to format output
+
+System.out.printf("%1.2f", amount);
+
+userInput = Text10.getlnInt();
+
+
+
